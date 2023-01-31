d3c4bb7b39b5522328dda667f311b1f191291a6d
Saksham Jindal <sakshamjindal3@gmail.com>
2023-01-31 17:22:27 +0530

    Seconf Day Nodejs

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require("express");
-
+const connectDB = require('./db')
 const app = express();
-
+connectDB()
+require('dotenv').config()
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("App up and running"));
 
+// post
+
+app.post('/process_post',(req, res)  => res.send("App up and not running")); 
+
+   
+
 // setting up port
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`App is running on http://localhost:${PORT}`);
-});
+
+// const db = process.env.mongo_uri;
+
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(db, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log("MongoDB is connected");
+//     } catch (err) {
+//         console.error(err.message);
+//         process.exit(1);
+//     }
+// };
+
+// 
